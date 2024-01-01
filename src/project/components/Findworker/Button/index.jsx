b48c7ddcdce825fcce68638cf9a7ba3a935a4f01
Vincent23412 <87458133+Vincent23412@users.noreply.github.com>
2024-01-01 16:40:49 +0800

    update

--- src/project/components/Findworker/Button/index.jsx
+++ src/project/components/Findworker/Button/index.jsx
@@ -8,9 +8,9 @@ import Typography from '@mui/material/Typography';
 export default function UserButton() {
   return (
     <Stack direction="row" spacing={2}>
-      <Custom_Button link='https://script.google.com/macros/s/AKfycby_fv2NyZLxcCkWDOiArIOMIbG8SgE8JgCUjZs0CZUWPZ8334L2s5kum99zbB1O-oxp/exec' word='post at'/>
+      <Custom_Button link='https://docs.google.com/forms/d/e/1FAIpQLSfNWRPc4tS6qnJVWjo_knh232EOkoSondfwXupkzCwKzcqv7w/viewform' word='post at'/>
 
-      <Custom_Button link='https://docs.google.com/forms/u/1/d/e/1FAIpQLSdIj3EKvGmdg_olWN_BVre1lp4nxEFnPmsVTF6_bf6AhaTchQ/viewform' word='register'/>
+      <Custom_Button link='https://docs.google.com/forms/d/e/1FAIpQLSdIj3EKvGmdg_olWN_BVre1lp4nxEFnPmsVTF6_bf6AhaTchQ/viewform' word='register'/>
     </Stack>
   );
 }
